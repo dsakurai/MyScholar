22151a1d9bfd4f1d7e1f96a4e06ede70506a4b3b
Daisuke Sakurai <d.sakurai0@gmail.com>
2024-01-17 19:34:50 +0900

    Initial Commit

---
+++ MyScholar/MyScholarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyScholarApp.swift
+//  MyScholar
+//
+//  Created by Daisuke Sakurai on 2024/01/17.
+//
+
+import SwiftUI
+
+@main
+struct MyScholarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
