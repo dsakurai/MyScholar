926b77356446d781430c242679130a808c80f465
Daisuke Sakurai <d.sakurai0@gmail.com>
2024-01-18 20:49:21 +0900

    Remove unused func

--- MyScholar/ContentView.swift
+++ MyScholar/ContentView.swift
@@ -11,23 +11,6 @@ import WebKit
 
 import SwiftSoup
 
-func getCharSet(httpResponse: HTTPURLResponse) -> String? {
-    
-    if let contentType = httpResponse.allHeaderFields["Content-Type"] as? String {
-        // Extract encoding from contentType, e.g., "text/html; charset=UTF-8"
-        
-        // Get the Charset field
-        let regex = try! NSRegularExpression(pattern: "charset=([^;]+)", options: .caseInsensitive)
-        
-        if let match = regex.firstMatch(in: contentType, options: [], range: NSRange(location: 0, length: contentType.utf16.count)),
-           let range = Range(match.range(at: 1), in: contentType) {
-               return String(contentType[range])
-        }
-    }
-    
-    return nil
-}
-
 struct WebView: NSViewRepresentable {
     let urlString: String
     
