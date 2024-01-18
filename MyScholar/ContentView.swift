05e13e4f762e81ceb7d4c02678b556b5603e4e1a
Daisuke Sakurai <d.sakurai0@gmail.com>
2024-01-18 20:48:52 +0900

    Add SwiftSoup; Pass HTML string to the content view.

--- MyScholar/ContentView.swift
+++ MyScholar/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 import WebKit
 
+import SwiftSoup
+
 func getCharSet(httpResponse: HTTPURLResponse) -> String? {
     
     if let contentType = httpResponse.allHeaderFields["Content-Type"] as? String {
@@ -28,24 +30,78 @@ func getCharSet(httpResponse: HTTPURLResponse) -> String? {
 
 struct WebView: NSViewRepresentable {
     let urlString: String
+    
+    @ObservedObject var webViewState: WebViewState
 
     func makeNSView(context: Context) -> WKWebView {
-        return WKWebView()
+        let webView = WKWebView()
+        
+        webView.navigationDelegate = context.coordinator
+        
+        return webView
     }
 
     func updateNSView(_ nsView: WKWebView, context: Context) {
+        
         if let url = URL(string: urlString) {
             let request = URLRequest(url: url)
             nsView.load(request)
         }
     }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: WebView
+        
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+                
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            webView.evaluateJavaScript("document.documentElement.outerHTML.toString()") {
+                (html, error) in
+                
+                if let htmlString = html as? String {
+                    // Now htmlString contains the entire HTML content of the page
+                    self.parent.webViewState.updateTitle(htmlString)
+                } else if let error = error {
+                    // Handle the error
+                    print("Error getting HTML string: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+}
+
+class WebViewState: ObservableObject {
+    @Published var html_string: String = ""
+    
+    func updateTitle(_ html: String) {
+        html_string = html
+    }
 }
 
 
 struct ContentView: View {
+    
+    @StateObject var webViewState = WebViewState()
+    
     var body: some View {
-        WebView(urlString: "https://scholar.google.co.jp/scholar?hl=en&as_sdt=0%2C5&q=test&btnG=")
+        HStack {
+            WebView(
+                urlString: "https://scholar.google.co.jp/scholar?hl=en&as_sdt=0%2C5&q=test&btnG=",
+                webViewState: webViewState
+            )
             .padding()
+            
+            Button("Test") {
+                print("hello")
+                print(webViewState.html_string)
+            }
+        }
     }
 }
 
